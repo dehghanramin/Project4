3578926d0b184fe2d41e803d7c84551495dbdc56
Ramin Dehghan <ramind123@yahoo.co.uk>
2019-04-24 20:29:58 -0700

    Added skeleton

--- Referee.cpp
+++ Referee.cpp
@@ -1,1 +1,10 @@
 #include "Referee.hpp"
+
+CReferee::CReferee(std::string const& id_, std::string const& firstname_, std::string const& lastname_, RefereeGrade const& grade_) :
+id(id_),
+firstname(firstname_),
+lastname(lastname_),
+grade(grade_) {}
+
+CReferee::~CReferee() {}
+
--- Referee.hpp
+++ Referee.hpp
@@ -1,4 +1,19 @@
 #ifndef _REFEREE_HPP_
 #define _REFEREE_HPP_
+#include <string>
+
+enum RefereeGrade {UNKNOWN, CLUB, STATE, NATIONAL, FIFA};
+
+class CReferee
+{
+    public:
+        CReferee(std::string const& = "R000", std::string const& = "None", std::string const& = "None", RefereeGrade const& = UNKNOWN);
+        ~CReferee();
+    private:
+        std::string id;
+        std::string firstname;
+        std::string lastname;
+        RefereeGrade grade;
+};
 
 #endif
